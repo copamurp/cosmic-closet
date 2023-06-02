f26d7c0011c5e3d64898672dcbe22a372e488f7a
Connor Murphy <themurfy@icloud.com>
2023-06-02 12:11:57 -0400

    minor: Carousel style changes
    Add children prop for passing a collection of testimonials.
    
    Minor Style changes.
    
    Remove code pertaining to testimonials. This code was deleted or refactored for use with the Testimonial component.

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 const StyledCarousel = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
   .carousel {
 	position: relative;
 	width: 100vw;
@@ -16,55 +20,17 @@ const StyledCarousel = styled.div`
 	  overflow: hidden;
 	  width: 100%;
 	  height: 100%;
-	  padding: 0;
-
-	  .carousel-item {
-		position: relative;
-		width: 100%;
-		height: 100%;
-		transition: transform 0.5s ease-in-out;
-		overflow: hidden;
-
-		.carousel-caption {
-		  position: absolute;
-		  top: 0;
-		  right: 0;
-		  bottom: 0;
-		  left: 0;
-		  z-index: 10;
-
-		  display: flex;
-		  flex-direction: column;
-		  justify-content: center;
-
-		  color: #ffffff;
-		  text-align: center;
-		  text-shadow: 0 0 2px #000000;
-		  background-color: rgba(0, 0, 0, 0.5);
-		  padding: 2rem;
-		  margin: 0;
-		  box-sizing: border-box;
-		}
-	  }
+	  padding: 2rem;
 	}
   }
 `;
 
-const Carousel = () => {
+const Carousel = ({children}) => {
 	return (
 		<StyledCarousel>
 			<div className={'carousel'}>
 				<div className={'carousel-inner'}>
-					<div className={'carousel-item active'}>
-						<div className={'carousel-caption'}>
-							<p>Lorum ipsum dolor sit amet, consectetur adipiscing elit.
-								Aliquam euismod, nunc nec aliquam ultricies, nunc elit
-								ultricies nunc, nec aliquam nunc elit nec nunc.
-								Aliquam euismod, nunc nec aliquam ultricies, nunc elit
-								ultricies nunc, nec aliquam nunc elit nec nunc.
-							</p>
-						</div>
-					</div>
+					{children}
 				</div>
 			</div>
 		</StyledCarousel>
