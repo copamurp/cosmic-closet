2cc82a21d5856b547fce5a9fce7db00641050ee3
Connor Murphy <themurfy@icloud.com>
2023-06-03 09:46:20 -0400

    Component previews

--- client/src/dev/previews.jsx
+++ client/src/dev/previews.jsx
@@ -3,6 +3,7 @@ import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import Nav from "../components/Nav";
 import Guest from "../components/Guest";
+import Testimonial from "../components/Testimonial";
 
 const ComponentPreviews = () => {
 	return (
@@ -12,6 +13,9 @@ const ComponentPreviews = () => {
 			</ComponentPreview>
 			<ComponentPreview path="/Guest">
 				<Guest/>
+			</ComponentPreview>
+			<ComponentPreview path="/Testimonial">
+				<Testimonial/>
 			</ComponentPreview>
 		</Previews>
 	)
