846a84577a268e0cd602dfcc8e2da56fc1674445
Connor Murphy <themurfy@icloud.com>
2023-06-03 09:45:44 -0400

    minor: config.js changes
    Add an object NETWORK_STATUS for containing network status information for use during API calls.
    
    Rename baseUrl to BASE_URL.
    
    Add module.exports

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,21 @@
-export const baseUrl = "http://localhost:3005/";
+const BASE_URL = "http://localhost:3005/";
+
+const NETWORK_STATUS = {
+	DEFAULT: {
+		code: 0,
+		description: 'Default'
+	},
+	GOOD: {
+		code: 1,
+		description: 'Good'
+	},
+	FAULT: {
+		code: -1,
+		description: 'Bad'
+	}
+}
+
+module.exports = {
+	BASE_URL,
+	NETWORK_STATUS
+}
