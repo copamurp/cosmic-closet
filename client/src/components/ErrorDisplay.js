dbfdd7a354fcd40c59e49ebf13cc547b0906b73a
Connor Murphy <themurfy@icloud.com>
2023-06-03 10:45:33 -0400

    ErrorDisplay.js style changes

--- client/src/components/ErrorDisplay.js
+++ client/src/components/ErrorDisplay.js
@@ -15,29 +15,31 @@ const StyledError = styled.div`
   max-height: 750px;
 
   .terminal {
-	background-color: rgba(7, 7, 7, 0.5);
-	flex: 1;
-	width: 100%;
-	height: 100%;
-	padding: 1rem;
-	border-radius: 0.25rem;
-	box-shadow: 0 0 3px 1px rgba(7, 7, 7, 0.8);
-
-	.error-code {
-	  font-size: 1rem;
-	}
-
-	.error-message {
-	  font-size: 1rem;
-	  height: 90%;
-	  display: flex;
-	  align-items: center;
-	  justify-content: center;
-	  background-color: rgba(0, 0, 0, 0.4);
+	  background-color: rgba(7, 7, 7, 0.5);
+	  flex: 1;
+	  width: 100%;
+	  height: 100%;
+	  padding: 1rem;
 	  border-radius: 0.25rem;
-	  box-shadow: 0 0 1px rgba(7, 7, 7, 0.8);
-	  padding: 2rem;
-	}
+	  box-shadow: 0 0 3px 1px rgba(7, 7, 7, 0.8);
+
+	  .error-code {
+		  font-size: 1rem;
+		  margin-bottom: 0.5rem;
+	  }
+
+	  .error-message {
+		  font-size: 1rem;
+		  height: 90%;
+		  display: flex;
+		  align-items: center;
+		  justify-content: center;
+		  background-color: rgba(0, 0, 0, 0.4);
+		  border-radius: 0.25rem;
+		  box-shadow: 0 0 1px rgba(7, 7, 7, 0.8);
+		  padding: 2rem;
+		  margin-bottom: 0.5rem;
+	  }
   }
 `;
 
