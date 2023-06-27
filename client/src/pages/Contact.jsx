0340489a25319ddfacf2a8458813282273f6c4f0
Connor Murphy <themurfy@icloud.com>
2023-06-27 11:51:25 -0400

    minor: Contact style changes
    Adjust the appearance of the contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,299 +1,323 @@
-import React from 'react';
-import styled from 'styled-components';
-import Loading from "../components/Loading";
-import getRandomError from "../helper/getRandomError";
-import ErrorDisplay from "../components/ErrorDisplay";
-import NETWORK_STATUS from "../config.js";
+import React from 'react'
+import styled from 'styled-components'
+import Loading from '../components/Loading'
+import getRandomError from '../helper/getRandomError'
+import ErrorDisplay from '../components/ErrorDisplay'
+import NETWORK_STATUS from '../config.js'
 
 const StyledContact = styled.div`
+  width: 100%;
+
+  h1 {
+    text-align: center;
+    color: #ffffff;
+    font-size: 2rem;
+    font-size: clamp(2rem, 1.8rem + 0.75vw, 3rem);
+    font-weight: 600;
+    margin: 2rem 0;
+  }
+
+  .form-wrapper {
+    height: 100%;
     width: 100%;
-    min-height: 80vh;
+    margin: 0 auto;
+    padding: 1rem;
+    min-height: 60vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    background: radial-gradient(ellipse at bottom, #5B5AA8 25%, #4f4ddf 100%);
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5) inset;
+  }
 
-    h1 {
-        text-align: center;
-        color: #ffffff;
-        font-size: 2rem;
-        font-size: clamp(2rem, 1.8rem + 0.75vw, 3rem);
-        font-weight: 600;
-        margin: 2rem 0;
+  .contact-form {
+    flex: 1;
+    width: 100%;
+    max-width: 800px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: space-evenly;
+
+    @media only screen and (max-width: 768px) {
+      flex-direction: column;
     }
 
-    .form-wrapper {
-        padding: 1rem;
-        min-height: 50vh;
-        height: 100%;
-        width: 100%;
-        background: radial-gradient(ellipse at bottom, #5B5AA8 25%, #4f4ddf 100%);
-        box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5) inset;
-    }
+    .info-container {
+      width: 50%;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: space-evenly;
 
-    .contact-form {
-        width: 100%;
-        max-width: 800px;
-        height: 100%;
+      @media only screen and (max-width: 768px) {
         margin: 0 auto;
-        display: flex;
-
-        @media only screen and (max-width: 768px) {
-            flex-direction: column;
-        }
-
-        > div {
-            width: 50%;
-            margin: 0 2.5%;
-            display: flex;
-            flex-direction: column;
-
-            @media only screen and (max-width: 768px) {
-                margin: 0 auto;
-                width: 100%;
-
-                :not(:last-child) {
-                    float: none;
-                }
-
-                :last-child {
-                    float: none;
-                }
-            }
-
-            :not(:last-child) {
-                float: left;
-            }
-
-            :last-child {
-                float: right;
-            }
-        }
-    }
-
-    .submit-wrapper {
         width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 1rem;
 
-        input[type='submit'] {
-            margin-top: 1rem;
-            padding: 1rem 2rem;
-            background-color: #202020;
-            color: #ffffff;
-            font-size: 1rem;
-            font-weight: 400;
-            border: 1px solid #4f4ddf;
-            border-radius: 0.5rem;
-            cursor: pointer;
-            box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-            transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out, border 0.5s ease-in-out;
-
-            :hover {
-                background-color: #ffffff;
-                color: #202020;
-                border: 1px solid #4f4ddf;
-            }
+        :not(:last-child) {
+          float: none;
         }
+
+        :last-child {
+          float: none;
+        }
+      }
+
+      :not(:last-child) {
+        float: left;
+      }
+
+      :last-child {
+        float: right;
+      }
     }
-`;
+
+    .message-container {
+      width: 80%;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: space-evenly;
+
+      @media only screen and (max-width: 768px) {
+        margin: 0 auto;
+        width: 100%;
+
+        :not(:last-child) {
+          float: none;
+        }
+
+        :last-child {
+          float: none;
+        }
+      }
+    }
+  }
+
+  input[type='submit'] {
+    padding: 1rem;
+    margin-top: 1rem;
+    padding: 1rem 2rem;
+    background-color: #202020;
+    color: #ffffff;
+    font-size: 1rem;
+    font-weight: 400;
+    border: 1px solid #4f4ddf;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+    transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out, border 0.5s ease-in-out;
+
+    :hover {
+      background-color: #ffffff;
+      color: #202020;
+      border: 1px solid #4f4ddf;
+    }
+  }
+`
 
 const StyledFormControl = styled.div`
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 
-    label {
-        color: #ffffff;
-        font-size: 1rem;
-        font-weight: 200;
-        margin-bottom: 0.25rem;
+  label {
+    color: #ffffff;
+    font-size: 1rem;
+    font-weight: 200;
+    margin-bottom: 0.25rem;
 
-        @media only screen and (max-width: 768px) {
-            font-size: 1rem;
+    @media only screen and (max-width: 768px) {
+      font-size: 1rem;
 
-            &::after {
-                content: '*';
-                color: #ff0000;
-            }
-        }
+      &::after {
+        content: '*';
+        color: #ff0000;
+      }
+    }
+  }
+
+  .wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+    margin: 0 auto;
+    padding: 0.5rem;
+    background-color: #181717;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+    border-radius: 0.25rem;
+
+    @media only screen and (max-width: 768px) {
+      padding: 1rem;
     }
 
-    .wrapper {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        justify-content: flex-start;
-        margin: 0 auto;
-        padding: 0.5rem;
-        background-color: #181717;
-        box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-        border-radius: 0.25rem;
+    input {
+      width: 100%;
+      padding: 0.5rem;
+      font-size: 1rem;
+      font-weight: 200;
+      border: 1px solid #4f4ddf;
+      border-radius: 0.25rem;
+      background-color: #202020;
+      color: #ffffff;
+      transition: border-color 0.5s ease-in-out;
 
-        @media only screen and (max-width: 768px) {
-            padding: 1rem;
-        }
+      :hover {
+        border-color: #ffffff;
+      }
 
-        input {
-            width: 100%;
-            padding: 0.5rem;
-            font-size: 1rem;
-            font-weight: 200;
-            border: 1px solid #4f4ddf;
-            border-radius: 0.25rem;
-            background-color: #202020;
-            color: #ffffff;
-            transition: border-color 0.5s ease-in-out;
+      :focus {
+        outline: none;
+      }
+    }
+  }
 
-            :hover {
-                border-color: #ffffff;
-            }
+  .message {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0.5rem;
+    background-color: #181717;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+    border-radius: 0.25rem;
 
-            :focus {
-                outline: none;
-            }
-        }
+    @media only screen and (max-width: 768px) {
+      padding: 1rem;
     }
 
-    .message {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        justify-content: flex-start;
-        width: 100%;
-        margin: 0 auto;
-        padding: 0.5rem;
-        background-color: #181717;
-        box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-        border-radius: 0.25rem;
-
-        @media only screen and (max-width: 768px) {
-            padding: 1rem;
-        }
-
-        textarea {
-            resize: none;
-            width: 100%;
-            height: 100%;
-            padding: 0.5rem;
-            font-size: 1rem;
-            font-weight: 200;
-            border: 1px solid #4f4ddf;
-            border-radius: 0.25rem;
-            background-color: #202020;
-            color: #ffffff;
-            transition: border-color 0.5s ease-in-out;
-            min-height: 200px;
-        }
+    textarea {
+      resize: none;
+      width: 100%;
+      height: 100%;
+      padding: 0.5rem;
+      font-size: 1rem;
+      font-weight: 200;
+      border: 1px solid #4f4ddf;
+      border-radius: 0.25rem;
+      background-color: #202020;
+      color: #ffffff;
+      transition: border-color 0.5s ease-in-out;
+      min-height: 200px;
     }
+  }
 
-    &#message {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-    }
-`;
+  &#message {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+  }
+`
 
 class Contact extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            contactStatus:   NETWORK_STATUS.DEFAULT,
+            contactStatus: NETWORK_STATUS.DEFAULT,
             showContactForm: true,
-            loading:         false,
-            randomError:     getRandomError(),
-        };
+            loading: false,
+            randomError: getRandomError(),
+        }
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.sendMail     = this.sendMail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.sendMail = this.sendMail.bind(this)
     }
 
     handleSubmit = async (event) => {
-        event.preventDefault();
+        event.preventDefault()
         this.setState({
             showContactForm: false,
-            loading:         true,
-        });
-        this.sendMail({message: "hi"})
-                .then(res => {
-                    clearTimeout(res[0]);
-                    this.setState({contactStatus: NETWORK_STATUS.GOOD});
-                })
-                .catch(() => {
-                    this.setState({contactStatus: NETWORK_STATUS.FAULT});
-                }).finally(() => this.setState({loading: false}));
+            loading: true,
+        })
+        this.sendMail({message: 'hi'})
+            .then(res => {
+                clearTimeout(res[0])
+                this.setState({contactStatus: NETWORK_STATUS.GOOD})
+            })
+            .catch(() => {
+                this.setState({contactStatus: NETWORK_STATUS.FAULT})
+            }).finally(() => this.setState({loading: false}))
     }
 
     sendMail = async ({message}) => {
-        this.setState({loading: true});
+        this.setState({loading: true})
 
-        await timeout(3500);
+        await timeout(3500)
 
-        return message;
+        return message
     }
 
     render() {
         return (
-                <StyledContact>
-                    <h1>Contact Us</h1>
-                    {this.state.showContactForm ?
-                            <div className={'form-wrapper'}>
-                                <form className={'contact-form'}
-                                      onSubmit={this.handleSubmit}>
-                                    <div>
-                                        <StyledFormControl>
-                                            <div className={'wrapper'}>
-                                                <label>First Name</label>
-                                                <input type={'text'}/>
-                                            </div>
-                                        </StyledFormControl>
-
-                                        <StyledFormControl>
-                                            <div className={'wrapper'}>
-                                                <label>Last Name</label>
-                                                <input type={'text'}/>
-                                            </div>
-                                        </StyledFormControl>
-
-                                        <StyledFormControl>
-                                            <div className={'wrapper'}>
-                                                <label>E-mail</label>
-                                                <input type={'text'}/>
-                                            </div>
-                                        </StyledFormControl>
+            <StyledContact>
+                <h1>Contact Us</h1>
+                {this.state.showContactForm ?
+                    <div className={'form-wrapper'}>
+                        <form className={'contact-form'}
+                              onSubmit={this.handleSubmit}>
+                            <div className={'info-container'}>
+                                <StyledFormControl>
+                                    <div className={'wrapper'}>
+                                        <label>First Name</label>
+                                        <input type={'text'}/>
                                     </div>
+                                </StyledFormControl>
 
-                                    <div>
-                                        <StyledFormControl id={'message'}>
-                                            <div className={'message'}>
-                                                <label>Message</label>
-                                                <textarea/>
-                                            </div>
-                                        </StyledFormControl>
+                                <StyledFormControl>
+                                    <div className={'wrapper'}>
+                                        <label>Last Name</label>
+                                        <input type={'text'}/>
                                     </div>
-                                </form>
+                                </StyledFormControl>
 
-                                <div className={'submit-wrapper'}>
-                                    <input type={'submit'}
-                                           value={'Submit'}/>
-                                </div>
+                                <StyledFormControl>
+                                    <div className={'wrapper'}>
+                                        <label>E-mail</label>
+                                        <input type={'text'}/>
+                                    </div>
+                                </StyledFormControl>
                             </div>
+
+                            <div className={'message-container'}>
+                                <StyledFormControl id={'message'}>
+                                    <div className={'message'}>
+                                        <label>Message</label>
+                                        <textarea/>
+                                    </div>
+                                </StyledFormControl>
+                            </div>
+                        </form>
+
+                        <input type={'submit'}
+                               value={'Submit'}/>
+                    </div>
+
+                    :
+                    <>
+                        {this.state.loading ?
+                            <Loading/>
                             :
-                            <>
-                                {this.state.loading ?
-                                        <Loading/>
-                                        :
-                                        <ErrorDisplay
-                                                error={this.state.randomError}/>
-                                }
-                            </>
-                    }
-                </StyledContact>
+                            <ErrorDisplay
+                                error={this.state.randomError}/>
+                        }
+                    </>
+                }
+            </StyledContact>
         )
     }
 }
 
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export default Contact;
+export default Contact
